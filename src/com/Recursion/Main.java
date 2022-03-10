b01c3ffbea756e96a3f0d2384a939df73bdb620d
Nitin <nsinghboss02@gmail.com>
2022-03-10 22:58:54 +0530

    Initial commit

---
+++ src/com/Recursion/Main.java
@@ -0,0 +1,18 @@
+package com.Recursion;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	// Treemap implementation.
+        /*
+        It is same as tree only the difference is ,now the kwy value pair will be
+        sorted in this.
+         */
+        Map <String,Integer> tmp = new TreeMap<>();
+        tmp.put("nitin",20);
+        tmp.put("nitish",21);
+        tmp.put("alok",23);
+        tmp.put("ravit",24);
+
+    }
+}
